03f42ecdaebabfb3f15ce4ca8235630d9d397f32
Lewandy Dilone <lewandydilone1@live.com>
2021-02-10 19:46:29 -0400

    ref: using foote component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -13,16 +14,8 @@ export default function Home() {
 
       <Header />
       <Main />
+      <Footer />
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Dominican Republic
-        </a>
-      </footer>
     </div>
   );
 }
