54da2b9edd5c72a933962c498d388b274a1a69d5
Lewandy Dilone <lewandydilone1@live.com>
2021-02-11 21:36:36 -0400

    remove next image optimization

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <section id="about" className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image className={styles.image} layout="intrinsic" width="400" height="300" src="/me2.jpeg" alt="Selfie picture" />
+        <img className={styles.image} width="400" height="300" src="/me2.jpeg" alt="Selfie picture" />
       </div>
       <div>Text</div>
     </section>
