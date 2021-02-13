2b3e15ef0ea80c1655d7bc75b308f46d8a0b99a9
Lewandy Dilone <lewandydilone1@live.com>
2021-02-12 21:10:25 -0400

    wip: about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,25 @@
-import Image from "next/image";
 import styles from "../styles/About.module.css";
 
 export default function About() {
   return (
     <section id="about" className={styles.container}>
       <div className={styles.imageContainer}>
-        <img className={styles.image} width="400" height="300" src="/me2.jpeg" alt="Selfie picture" />
+        <img
+          className={styles.image}
+          src="/me2.jpeg"
+          alt="Selfie picture"
+        />
       </div>
-      <div>Text</div>
+      <div className={styles.summaryContainer}>
+        <h1>Summary</h1>
+
+        <p>
+          I am a Developer passionate for building complete web solutions. I
+          mainly focus on front end technologies including but not limited to
+          <strong>Vue</strong> and <strong>React</strong>. I am also proficient
+          in the back end using NodeJs.
+        </p>
+      </div>
     </section>
   );
 }
